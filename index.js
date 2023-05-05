141f718a559da1db01b7f40ccae5fb977d72a367
Gaurav Rawat <gauravrawat202122@gmail.com>
2023-05-05 16:43:11 +0530

    updated cors

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ const app = express();
 // app.use(
 //   cors({ credentials: true, origin: 'https://insight-api-7biz.onrender.com' })
 // );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
